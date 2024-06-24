31e85abebc76b318c53dc6be4ab83a8dc77affa5
Alex Rudshild <97178056+AlexRudshild@users.noreply.github.com>
2024-06-25 01:17:32 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CharacterAI Translator
 // @namespace    This script automatically translates chat messages to the user's local language.
-// @version       1.0.1
+// @version       1.0.2
 // @description  try to take over the world!
 // @author       titanRGB
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
@@ -14,7 +14,6 @@
 
 "use strict";
 
-const icon = '<svg width="24" height="24" viewBox="0 0 24 24" focusable="false" class="ep0rzf NMm5M"><path d="M12.87 15.07l-2.54-2.51.03-.03A17.52 17.52 0 0 0 14.07 6H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z"></path></svg>';
 const translate_button = '<button class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk revert-translate-btn" tabindex="0" type="button" style="fill: white; --darkreader-inline-fill: #e5e0d8;" data-darkreader-inline-fill=""><svg width="24" height="24" viewBox="0 0 24 24" focusable="false" class="ep0rzf NMm5M"><path d="M12.87 15.07l-2.54-2.51.03-.03A17.52 17.52 0 0 0 14.07 6H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z"></path></svg></button>';
 
 let debug = false;
