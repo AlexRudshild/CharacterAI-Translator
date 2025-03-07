efedc3752aae8ad1a7da3c3d95b95a662e8495c3
Alex Rudshild <97178056+AlexRudshild@users.noreply.github.com>
2025-03-07 23:53:02 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name         CharacterAI Translator
 // @namespace    This script automatically translates chat messages to the user's local language.
-// @version       1.0.6
+// @version       1.0.7
 // @description  try to take over the world!
 // @author       titanRGB
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @match        https://*.character.ai/*
 // @match        https://*.crushon.ai/*
+// @match        https://*.janitorai.com/*
 // @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM.registerMenuCommand
+// @grant        GM_registerMenuCommand
 // @run-at       document-end
 // @downloadURL https://raw.githubusercontent.com/AlexRudshild/CharacterAI-Translator/main/code.js
 // @updateURL https://raw.githubusercontent.com/AlexRudshild/CharacterAI-Translator/main/code.js
@@ -17,23 +22,86 @@
 
 const translate_icon =
 	'<svg width="24" height="24" viewBox="0 0 24 24" focusable="false" class="ep0rzf NMm5M"><path d="M12.87 15.07l-2.54-2.51.03-.03A17.52 17.52 0 0 0 14.07 6H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z"></path></svg>';
-const translate_button = `<button class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk revert-translate-btn" tabindex="0" type="button" style="fill: white; --darkreader-inline-fill: #e5e0d8;" data-darkreader-inline-fill="">${translate_icon}</button>`;
 
 let debug = false;
 let current_message = null;
+let token = GM_getValue("GPT_Token");
+let translator = GM_getValue("translator") ?? "google";
+
+function menuItem1() {
+	token = prompt("Input gpt api token");
+	GM_setValue("GPT_Token", token);
+}
+
+function menuItem2() {
+	translator = "google";
+	GM_setValue("translator", translator);
+}
+
+function menuItem3() {
+	translator = "gpt";
+	GM_setValue("translator", translator);
+}
+
+GM_registerMenuCommand("Set GPT Token", menuItem1);
+GM_registerMenuCommand("Set Google Translator", menuItem2);
+GM_registerMenuCommand("Set GPT Translator", menuItem3);
 
 const sites = {
+	["janitorai.com"]: {
+		add_translate_button: () => {
+			const targets = document.querySelectorAll("div[data-index]");
+			for (let i = targets.length - 1; i >= 0; i--) {
+				const target = targets[i].querySelector("li");
+				const buttonHolder = target?.children.length > 1 ? target?.children[1] : target?.children[0];
+				if (!buttonHolder) continue;
+				const targetDiv = buttonHolder.children[1].children[0];
+
+				let buttonClone = targetDiv.querySelector(`.revert-translate-btn`);
+
+				if (buttonClone !== null) continue;
+
+				buttonClone = document.createElement("button");
+				buttonClone.className = "MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk revert-translate-btn";
+				buttonClone.classList.add("revert-translate-btn");
+				buttonClone.style.fill = "white";
+				targetDiv.appendChild(buttonClone);
+				buttonClone.innerHTML = translate_icon;
+				buttonClone.addEventListener("click", async (e) => {
+					toggleAutoTranslate(buttonHolder.children[1].children[1]);
+				});
+			}
+		},
+		add_to_auto_translate: () => {
+			const targets = document.querySelectorAll("div[data-index]");
+			for (let i = 0; i < targets.length; i++) {
+				const target = targets[i].querySelector("li");
+				const message = target?.children.length > 1 ? target?.children[1] : target?.children[0];
+				const node = message?.children[1].children[1];
+
+				if (!node || node.parentNode.hasAttribute("is_auto_translate")) {
+					continue;
+				}
+
+				node.parentNode.setAttribute("is_auto_translate", "true");
+
+				setInterval(async () => {
+					await translateNode(node);
+				}, 500);
+			}
+		},
+	},
 	["crushon.ai"]: {
 		add_translate_button: () => {
 			const targets = document.querySelectorAll('div[class="text-sm font-bold dark:text-white-1 double-click-filter"]');
 			for (let i = 0; i < targets.length; i++) {
 				const target = targets[i];
 				const buttonHolder = target?.children[0];
-				if (!buttonHolder) return;
+				if (!buttonHolder) continue;
 
 				var buttonClone = buttonHolder.querySelector(`.revert-translate-btn`);
 
-				if (buttonClone !== null) return;
+				if (buttonClone !== null) continue;
 
 				buttonClone = document.createElement("button");
 				buttonClone.className = "MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk revert-translate-btn";
@@ -44,7 +112,7 @@ const sites = {
 				buttonClone.addEventListener("click", async (e) => {
 					const message = e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
 					const node = message.querySelector('div[class="MarkdownText_CustomMarkdownText__P3bB6 not-prose w-full text-sm md:text-base"]');
-					const screen = document.querySelector('main').parentElement;
+					const screen = document.querySelector("main").parentElement;
 					const scroll_pos = screen.scrollTop;
 					toggleAutoTranslate(node);
 					screen.scrollTop = scroll_pos;
@@ -149,7 +217,7 @@ setInterval(() => {
 	} catch (error) {
 		console.error(error);
 	}
-}, 100);
+}, 500);
 
 function toggleAutoTranslate(node) {
 	node.isDisableTranslate = !node.isDisableTranslate;
@@ -170,7 +238,7 @@ async function subbToHover(message) {
 async function translateNode(node) {
 	let nodeContent = node.innerHTML;
 
-	if (node.isDisableTranslate || node.storeContent == nodeContent) return;
+	if (node.isDisableTranslate || CompareStrings(node.storeContent, nodeContent)) return;
 	if (node.oldContent == nodeContent) {
 		node.innerHTML = node.storeContent;
 		return;
@@ -180,12 +248,18 @@ async function translateNode(node) {
 	node.isTranslating = true;
 
 	let result = await translateHTML(nodeContent, "en", navigator.language);
-	result = result.replace(/\&quot\;/g, '"');
+	result = result.replace(/\&#?(quot|39)\;/g, '"');
 
 	node.oldContent = nodeContent;
 	node.innerHTML = result;
-	node.isTranslating = false;
 	node.storeContent = result;
+
+	node.isTranslating = false;
+}
+
+const STRING_WILD_CARD = /[<>= \/\n]|div/gm;
+function CompareStrings(str1 = "", str2 = "") {
+	return str1.toLowerCase().replace(STRING_WILD_CARD, "") === str2.toLowerCase().replace(STRING_WILD_CARD, "");
 }
 
 async function translateHTML(html, sLang, tLang) {
@@ -345,10 +419,6 @@ async function translate(text, sLang = "", tLang = "", format, key) {
 			console.log(`preTranslate: ${text}`);
 		}
 
-		if (format == null) {
-			format = "html";
-		}
-
 		if (key == null) {
 			key = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw";
 		}
@@ -359,27 +429,13 @@ async function translate(text, sLang = "", tLang = "", format, key) {
 			return cache[cacheKey];
 		}
 
-		const response = await new Promise((resolve, reject) => {
-			GM_xmlhttpRequest({
-				method: "POST",
-				url: `https://translate.googleapis.com/translate_a/t?client=gtx&format=${format}&sl=${sLang}&tl=${tLang}&key=${key}`,
-				data: `q=${encodeURIComponent(text)}`,
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-				onload: (response) => {
-					const json = JSON.parse(response.responseText);
-					if (Array.isArray(json[0])) {
-						resolve(json[0][0]);
-					} else {
-						resolve(json[0]);
-					}
-				},
-				onerror: (response) => {
-					reject(response.statusText);
-				},
-			});
-		});
+		let response;
+
+		if (translator === "gpt") {
+			response = await GptTranslate(text, sLang, tLang, token);
+		} else {
+			response = await GoogleTranslate(text, sLang, tLang, key);
+		}
 
 		cache[cacheKey] = response;
 
@@ -391,4 +447,65 @@ async function translate(text, sLang = "", tLang = "", format, key) {
 	} catch (error) {
 		console.error(error);
 	}
+}
+
+async function GoogleTranslate(text, sLang, tLang, key) {
+	const response = await new Promise((resolve, reject) => {
+		GM_xmlhttpRequest({
+			method: "POST",
+			url: `https://translate.googleapis.com/translate_a/t?client=gtx&format=html&sl=${sLang}&tl=${tLang}&key=${key}`,
+			data: `q=${encodeURIComponent(text)}`,
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			onload: (response) => {
+				const json = JSON.parse(response.responseText);
+				if (Array.isArray(json[0])) {
+					resolve(json[0][0]);
+				} else {
+					resolve(json[0]);
+				}
+			},
+			onerror: (response) => {
+				reject(response.statusText);
+			},
+		});
+	});
+
+	return response;
+}
+
+async function GptTranslate(text, sLang, tLang, key) {
+	const response = await new Promise((resolve, reject) => {
+		GM_xmlhttpRequest({
+			method: "POST",
+			url: `https://api.openai.com/v1/chat/completions`,
+			data: JSON.stringify({
+				model: "gpt-4o-mini",
+				messages: [
+					{
+						role: "developer",
+						content: `Ты переводчик ты должен только переводить текст на русский ничего кроме перевода не отвечай и не изменяй структуру элементов.`,
+					},
+					{
+						role: "user",
+						content: text,
+					},
+				],
+			}),
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+			onload: (response) => {
+				const json = JSON.parse(response.responseText);
+				resolve(json.choices[0].message.content);
+			},
+			onerror: (response) => {
+				reject(response.statusText);
+			},
+		});
+	});
+
+	return response;
 }
